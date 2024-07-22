266241a28d2693c252447c10272762bd9be4b551
TSRT5267 <tsrt5267@naver.com>
2024-07-22 23:36:36 +0900

    플리퍼 업뎃

--- Assets/FlipperScript.cs
+++ Assets/FlipperScript.cs
@@ -5,6 +5,7 @@ public class FlipperScript : MonoBehaviour
     [Header("Filpper")]
     [SerializeField] private KeyCode filpKey;
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float  flipperForce;
    
     
 
@@ -13,7 +14,7 @@ public class FlipperScript : MonoBehaviour
     {
         if(Input.GetKeyDown(filpKey))
         {           
-            rb.AddTorque(10000f);
+            rb.AddTorque(flipperForce);
         }
     }
 }
